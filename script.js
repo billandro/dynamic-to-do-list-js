c5e04f6938a5640072c346f774925211a8d726d2
Bill Qubeka <Bill Qubeka>
2024-07-16 11:23:16 +0200

    build a dynamic to-do list application

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Retreive the add button, task input and task list
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    // Function to add a task
+    function addTask() {
+        // Retreive the task input value
+        const taskText = document.getElementById("task-input").value.trim();
+        
+        if (taskText == "") {
+            alert("Please enter a task please:");
+        } else {
+            const listElement = document.createElement("li");
+            listElement.textContent = taskText;
+
+            const buttonElement = document.createElement("button");
+            buttonElement.textContent = "Remove";
+            buttonElement.classList.add("remove-btn");
+
+            // Remove list element from list onlclick
+            buttonElement.onclick = function(event) {
+                const parent = event.target.parentElement;
+                const unoderedL = parent.parentElement;
+
+                // Remove specific li element from ul element
+                unoderedL.removeChild(parent);
+            };
+
+            // Append button element to the li element
+            listElement.appendChild(buttonElement);
+            // Append the li element to the ul element
+            taskList.appendChild(listElement);
+
+            // Clear task input field
+            taskInput.value = "";
+        }
+    }
+
+    // When the add task button is clicked add task
+    addButton.addEventListener('click', addTask);
+
+    // When enter key is pressed add task
+    taskInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            addTask();
+        }
+    });
+
+    document.addEventListener("DOMContentLoaded", addTask);
+});
